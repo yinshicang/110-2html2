4be1ae00e06be240cd373cc428683ca5f24638f5
yinshicang <82023203+yinshicang@users.noreply.github.com>
2021-06-16 14:28:16 +0800

    Add files via upload

---
+++ 15.login.php
@@ -0,0 +1,20 @@
+<?php
+if($_GET["id"]=="gsdh"&&$_GET["pwd"]=="545454564")
+echo "login success";
+
+else
+echo"login fail";
+
+
+
+
+
+
+
+if($_POST["id"]=="gsdh"&&$_POST["pwd"]=="545454564")
+echo "login success";
+
+else
+echo"login fail";
+
+?><!--echo印出-->
